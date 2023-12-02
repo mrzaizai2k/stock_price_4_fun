7521c328c548018bd36eb3f4501dd0ef5a63dc96
Mai Chí Bảo <chibao24.12.1999@gmail.com>
2023-12-02 09:57:32 +0700

    Update stock_bot.py refactor

--- src/stock_bot.py
+++ src/stock_bot.py
@@ -5,7 +5,7 @@ import yaml
 import schedule
 import threading
 from threading import Thread
-from time import sleep
+import time
 import subprocess
 
 import telebot
@@ -14,7 +14,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # from telegram.ext.filters import Filters 
-from src.utils import *
+from Utils.utils import *
+from Utils.bot_utils import warning_macd, warningpricevsma, warningsnr, warningbigday, validate_symbol_decorator, run_vscode_tunnel
 from src.PayBackTime import PayBackTime, find_PBT_stocks
 from src.motif import MotifMatching, find_best_motifs
 from src.Indicators import MACD, BigDayWarning, PricevsMA
@@ -239,82 +240,6 @@ def analyze_winloss(message):
     bot.send_message(message.chat.id, report)
 
 
-def warning_macd(watchlist, user_id):  # Pass the message parameter
-
-    warning_report = []  # Initialize an empty list to store warning reports
-    for symbol in watchlist:
-        macd = MACD(symbol)
-        if macd.is_cross_up(offset=3):
-            warning_report.append(f'{symbol}: Crossed up 🔼')
-        elif macd.is_cross_down(offset=3):
-            warning_report.append(f'{symbol}: Crossed down 🔻')
-    if warning_report:
-        # If there are warnings, send a report
-        report_message = '\n'.join(warning_report)
-        return bot.send_message(user_id, f'Report for stocks with warnings:\n{report_message}')
-
-
-def warningbigday(watchlist, user_id):  # Pass the message parameter
-
-    warning_report = []  # Initialize an empty list to store warning reports
-
-    for symbol in watchlist:
-        bigday = BigDayWarning(symbol, percent_diff=3)
-        if bigday.is_big_increase():
-            warning_report.append(f'Powerful UP for {symbol} 💹')
-        if bigday.is_big_decrease():
-            warning_report.append(f'Powerful DOWN for {symbol} 🆘')
-
-    if warning_report:
-        # If there are FTD warnings, send a report
-        report_message = '\n'.join(warning_report)
-        return bot.send_message(user_id, f'Report for stocks with big day warnings:\n{report_message}')
-
-
-def warningpricevsma(watchlist, user_id):  # Pass the message parameter
-    warning_report = []  # Initialize an empty list to store warning reports
-
-    for symbol in watchlist:
-        pvma = PricevsMA(symbol)
-        pvma_offset = data.get('pvma_offset')
-
-        if pvma.is_cross_up(offset=pvma_offset):
-            warning_report.append(f'{symbol}: Crossed up')
-        elif pvma.is_cross_down(offset=pvma_offset):
-            warning_report.append(f'{symbol}: Crossed down')
-
-    if warning_report:
-        # If there are warnings, send a report
-        report_message = '\n'.join(warning_report)
-        return bot.send_message(user_id, f'Report for stocks with PricevsMA warnings:\n{report_message}')
-
-
-def warningsnr(watchlist, user_id):  # Pass the message parameter
-    warning_report = []  # Initialize an empty list to store warning reports
-    tolerance_percent = data.get('tolerance_percent') 
-    for symbol in watchlist:
-        sr_finding = SupportResistFinding(symbol=symbol)
-        current_price = sr_finding.get_current_price()
-
-        # Find the closest support and resistance levels
-        support, resistance = sr_finding.find_closest_support_resist(current_price=current_price)
-
-        # Set the tolerance percentage
-        tolerance_percent = tolerance_percent / 100  # 1 percent
-
-        # Check if the current price is within the tolerance range of support or resistance
-        if support <= current_price <= support * (1 + tolerance_percent):
-            warning_report.append(f'{symbol}: Meeting Support')
-        elif resistance is not None and (resistance * (1 - tolerance_percent) <= current_price <= resistance):
-            warning_report.append(f'{symbol}: Meeting Resistance')
-
-
-    if warning_report:
-        # If there are warnings, send a report
-        report_message = '\n'.join(warning_report)
-        return bot.send_message(user_id, f'Support/Resistance warning for stocks:\n{report_message}')
-
-
 @bot.message_handler(commands=['watchlist'])
 def handle_watchlist(message):
     markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
@@ -366,30 +291,13 @@ def process_remove_stock(message):
         bot.send_message(message.chat.id, f"{symbol} not found in your watchlist.")
 
 
-def run_vscode_tunnel(message):
-    command = ['./code', 'tunnel']
-    bot.reply_to(message, f"VS Code remote tunnel opened!: https://vscode.dev/tunnel/bao_msi/root/code_Bao/stock_price_4_fun")
-    result = subprocess.run(command, check=True, text=True)
-
 @bot.message_handler(commands=['remote'])
 def open_vscode_tunnel(message):
     bot.reply_to(message, f"VS Code remote tunnel Opening...")
-    Thread(target=run_vscode_tunnel, args=(message,)).start() 
+    Thread(target=run_vscode_tunnel, args=(bot, message)).start()
 
 
-def schedule_checker():
-    while True:
-        schedule.run_pending()
-        sleep(1)
-
-# Define the function to handle all other messages
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    response_message = "Apologies, I didn't understand that command. 😕\nPlease type /help to see the list of available commands."
-    bot.send_message(message.chat.id, response_message)
-
-def main():
-
+def warning_stock():
     times = data.get('times', []) 
     functions = [warning_macd, warningpricevsma, warningsnr, warningbigday]
     user_db = UserDatabase(user_data_path=user_data_path)
@@ -400,10 +308,23 @@ def main():
         for func in functions:
             for user in user_list:
                 watchlist = user_db.get_watch_list(user_id=user)
-                schedule.every().day.at(f"{time_str}").do(func, watchlist=watchlist, user_id=user)
+                if func in [warningsnr, warningpricevsma]:
+                    schedule.every().day.at(f"{time_str}").do(func, bot=bot , watchlist=watchlist, user_id=user, data_config = data)
+                else:
+                    schedule.every().day.at(f"{time_str}").do(func, bot=bot , watchlist=watchlist, user_id=user)
 
+            
+
+# Define the function to handle all other messages
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    response_message = "Apologies, I didn't understand that command. 😕\nPlease type /help to see the list of available commands."
+    bot.send_message(message.chat.id, response_message)
+
+def main():
+
+    warning_stock()
     schedule.every().day.at(data.get('scape_trading_data_time')).do(scape_trading_data, user_name = TRADE_USER, password = TRADE_PASS)
-
     Thread(target=schedule_checker).start() 
 
 
