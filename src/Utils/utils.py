7d9a0547480df96f8b6ea12ad43bada829e59546
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-01-20 17:57:24 +0700

    add timeit decorator

--- src/Utils/utils.py
+++ src/Utils/utils.py
@@ -14,6 +14,17 @@ from functools import wraps
 import torch
 from src.Microsofttodo import *
 
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} took {execution_time:.2f} seconds to execute.")
+        return result
+
+    return wrapper
+
 def create_env_file():
     env_file_path = '.env'
     if not os.path.isfile(env_file_path):
